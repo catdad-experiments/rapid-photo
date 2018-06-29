273e765870d63e0e3daa0e0eac2deca51a72460b
Kiril Vatev <vatev.1@gmail.com>
2018-06-29 16:41:21 -0400

    hooking up input controls

--- src/controls.js
+++ src/controls.js
@@ -6,8 +6,49 @@
   var controlsElem = document.querySelector('#controls');
   var captureBtn = document.querySelector('#capture');
 
+  function createInput(id, defaultVal) {
+    var elem = document.querySelector('#' + id);
+    var fieldElem = elem.querySelector('.field');
+    var lessElem = elem.querySelector('.less');
+    var moreElem = elem.querySelector('.more');
+
+    var value = Number(fieldElem.value);
+
+    function onMore() {
+      value += 1;
+      fieldElem.value = value;
+    }
+
+    function onLess() {
+      value -= 1;
+      fieldElem.value = value;
+    }
+
+    lessElem.addEventListener('click', onLess);
+    moreElem.addEventListener('click', onMore);
+
+    return Object.defineProperties({}, {
+      value: {
+        get: function () {
+          return value;
+        },
+        set: function (val) {
+          fieldElem.value = value = val;
+        }
+      },
+      destroy: {
+        value: function () {
+          lessElem.removeEventListener('click', onLess);
+          moreElem.removeEventListener('click', onMore);
+        }
+      }
+    });
+  }
+
   register(NAME, function () {
     var context = this;
+
+    var countControl = createInput('count');
 
     function onCaptureBtn() {
       // starting a new video will begin a capture
@@ -31,6 +72,8 @@
       captureBtn.removeEventListener('click', onCaptureBtn);
       context.events.off('capture-start', onCaptureStart);
       context.events.off('capture-end', onCaptureEnd);
+
+      countControl.destroy();
     };
   });
 }(window.registerModule));
